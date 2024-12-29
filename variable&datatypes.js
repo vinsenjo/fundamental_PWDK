e946c1f1731618e92c272336dd4a3fbb52715ce0
VinsenJovin <vinjovinda@gmail.com>
2024-12-29 11:22:10 +0700

    first commit

---
+++ variable&datatypes.js
@@ -0,0 +1,26 @@
+// Write a code to find area of rectangle.
+// Example : length = 5, width = 3
+// Output : 15
+
+function rectangle(length, width) {
+  return length * width;
+}
+
+// // Write a code to find perimeter of rectangle.
+// // ○ Example : length = 5, width = 3
+// ○ Output : 16
+
+
+// ● Write a code to find diameter, circumference and area of a circle.
+// ○ Example : radius = 5
+// ○ Output : diameter = 10, circumference = 31.4159, area = 78.539
+// ● Write a code to find angles of triangle if two angles are given.
+// ○ Example : a = 80, b = 65
+// ○ Output : 35
+// ● Write a code to convert days to years, months and days (Notes: 1 year : 365 days, 1 month : 30
+// days).
+// ○ Example : 400 days → 1 year, 1 month, 5 days
+// ○ Example: 366 days → 1 year, 0 month, 1 day
+// ● Write a code to get difference between dates in days.
+// ○ Example : date1 = 2022-01-20, date2 = 2022-01-22
+// ○ Output : 2
